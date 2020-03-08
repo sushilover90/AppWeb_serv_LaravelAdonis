1a2563ee6a94b2d46c50461881d4eea8b855459e
GerardoDM <geradelgado22@hotmail.com>
2020-03-07 23:48:35 -0600

    imprimiendo responses

--- resources/js/components/Triggers.vue
+++ resources/js/components/Triggers.vue
@@ -1,22 +1,57 @@
 <template>
+
+
     <div class="container">
-        <div class="row">
+
+
+
+        <h1>Triggers Response</h1>
+<!-- 
+         <ul>
+             <li v-for="(value, key) in respuesta.adonis_response" v-bind:key = key>{{ key }}: {{ value }} </li>
+        </ul> -->
+
+        <table class="table table-dark">
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Rule</th>
+      <th>Description</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="respuesta in respuesta.adonis_response" v-bind:key="respuesta.key">
+      <td>{{respuesta.name}}</td>
+      <td>{{respuesta.description}}</td>
+      <td>{{respuesta.rule}}</td>
+    </tr>
+  </tbody>
+</table>
+        <!-- <div class="row">
             <div class="col-12">
                 <div class="card">
                     <div class="card-header">
-                        Peticion triggers
+                       <h4>66</h4>
+                    </div> -->
+
+                     <!-- <div class="jumbotron jumbotron-fluid">
+                     <div class="container">
+                        <h1 class="display-4">{{adonis_response.body.andromouse}}</h1>
+                        <p class="lead"></p>
                     </div>
-                    <div class="card-body">
+                    </div> -->
+
+                    <!-- <div class="card-body">
                         <input type="button" class="btn bg-primary" value="ENVIAR PETICION" v-on:click="peticion">
                         <vue-json-pretty
                             :path="'res'"
                             :data="{ respuesta }"
                             @click="handleClick">
                         </vue-json-pretty>
-                    </div>
-                </div>
+                    </div> -->
+                <!-- </div>
             </div>
-        </div>
+        </div> --> 
     </div>
 </template>
 
@@ -27,25 +62,57 @@
     export default {
         data(){
             return{
-                respuesta: null
+                respuesta: []
             }
         },
         mounted() {
 
         },
+
+        created(){
+            this.traer();
+        } ,
         methods:{
-            peticion:function () {
+
+            traer(){
+
                 let self = this;
                 this.respuesta = null;
                 axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.laravel_token;
                 axios.get('/api/shodan/triggers').then(function (response) {
                     console.log(response.data);
                     self.respuesta = response.data;
+                    //AE
+                
+                    //
+                    console.log(self.respuesta.adonis_response);
+                    console.log(self.respuesta.adonis_response);
+
                 }).catch(function (response) {
 
                 });
             }
-        },
+
+            },
+
+            // peticion:function () {
+            //     let self = this;
+            //     this.respuesta = null;
+            //     axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.laravel_token;
+            //     axios.get('/api/shodan/protocols').then(function (response) {
+            //         console.log(response.data);
+            //         self.respuesta = response.data;
+            //         //AE
+            //         console.log(self.respuesta.adonis_response.ard);
+            //         //
+            //         console.log(self.respuesta.adonis_response);
+            //         console.log(self.respuesta.adonis_response);
+
+            //     }).catch(function (response) {
+
+            //     });
+            // }
+        
         components: {
             VueJsonPretty
         }
